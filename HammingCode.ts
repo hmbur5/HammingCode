1f5a189d65592ddb9fdd9df32a16e206b780269d
Hannah Burke <hmbur5@student.monash.edu>
2022-04-08 11:52:31 +1000

    Functionality for decoding and error checking

--- HammingCode.ts
+++ HammingCode.ts
@@ -21,7 +21,60 @@ function encode(data: string): string{
     return String(P) + String(H1) + String(H2) + String(H3) + data;
 }
 
-// Testing
-var myData: string = "1010";
-var myEncodedData: string = encode(myData);
-console.log(myEncodedData)
+
+
+
+
+/**
+ * Performs Hamming decoding on a string of 8-bit data to return the original 
+ * 4-bit data, correcting any errors that may results from a single bit being 
+ * flipped during transmission. 
+ * @param {string} encoded_data a string of 8 elements that are 1s or 0s, 
+ * representing the transmitted byte which may contain an error in one bit
+ * @returns {string} a string of 4 elements representing the original data
+ */
+function decode(encoded_data: string): string{
+
+    // recalculate Hamming bits
+    var D1: number = Number(encoded_data.charAt(4));
+    var D2: number = Number(encoded_data.charAt(5));
+    var D3: number = Number(encoded_data.charAt(6));
+    var D4: number = Number(encoded_data.charAt(7));
+    var H1: number = (D1+D2+D4)%2;
+    var H2: number = (D1+D3+D4)%2;
+    var H3: number = (D2+D3+D4)%2;
+
+    // compare to input
+    var H1check: boolean = H1==Number(encoded_data.charAt(1))
+    var H2check: boolean = H2==Number(encoded_data.charAt(2))
+    var H3check: boolean = H3==Number(encoded_data.charAt(3))
+
+    //iterate through possibilities of checks being false, swap appropriate 
+    // bit if required, and return original data. 
+
+    // if all wrong, swap bit at D4
+    if ((H1check || H2check || H3check) == false) {
+        return String(D1) + String(D2) + String(D3) + String(1-D4);
+    }
+
+    // if only H1 and H2 are wrong, swap bit at D1
+    else if ((H1check || H2check) == false) {
+        return String(1-D1) + String(D2) + String(D3) + String(D4);
+    }
+
+    // if only H1 and H3 are wrong, swap bit at D2
+    else if ((H1check || H3check) == false) {
+        return String(D1) + String(1-D2) + String(D3) + String(D4);
+    }
+
+    // if only H2 and H3 are wrong, swap bit at D3
+    else if ((H2check || H3check) == false) {
+        return String(D1) + String(D2) + String(1-D3) + String(D4);
+    }
+
+    // any other possibility means any error would be in a redundant bit, so 
+    //return data as is
+    else {
+        return String(D1) + String(D2) + String(D3) + String(D4);
+    }
+}
